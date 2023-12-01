7b9bc22fb466e617b6efb066238972857d65b680
Joel <joel@gluth.io>
2023-12-01 14:20:34 +0000

    Day 1 code complete

--- src/aoc_2023/day_1.clj
+++ src/aoc_2023/day_1.clj
@@ -1,2 +1,40 @@
 (ns aoc-2023.day-1
-  (:require [aoc-2023.core :refer :all]))
+  (:require [aoc-2023.core :refer :all]
+            [clojure.string :as str]))
+
+(defn first-and-last [astr]
+  (intify-seq
+   (let [rs (re-seq #"\d" astr)]
+     [(first rs) (last rs)])))
+
+(defn make-number [[a b]]
+  (+ (* 10 a) b))
+
+(defn -calibration-value [aseq fandl-f]
+  (reduce +
+          (map make-number
+               (map fandl-f aseq))))
+
+(defn calibration-value [aseq]
+  (-calibration-value aseq first-and-last))
+
+(def digits ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
+
+(defn wtoi [astr]
+  (.indexOf digits astr))
+
+(defn atoi [astr]
+  (let [n (wtoi astr)]
+    (if (= -1 n)
+      (Integer/parseInt astr)
+      n)))
+
+(defn first-and-last-with-words [astr]
+  (map atoi
+       ;; this looks ugly because you have to use lookahead groups to deal with the "oneight" case
+       (let [rs (map second
+                     (re-seq (re-pattern (str "(?=("(str/join "|" digits) "|[\\d]))")) astr))]
+     [(first rs) (last rs)])))
+
+(defn calibration-value-with-words [aseq]
+  (-calibration-value aseq first-and-last-with-words))
--- test/aoc_2023/day_1_test.clj
+++ test/aoc_2023/day_1_test.clj
@@ -4,7 +4,81 @@
             [aoc-2023.core :refer :all]
             [clojure.string :as str]))
 
-(deftest a-test
-  (testing "Start with a failing test"
-    (is (= false true))))
+(def example-data (str/split
+                   "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"
+                   #"\n"))
 
+(deftest first-last-test
+  (testing "Finding the first and last digit in a line"
+    (is (= [1 2]
+           (first-and-last (first example-data))))))
+
+(deftest nmake-number-test
+  (testing "Turning a pair of digits into a number"
+    (is (= 12
+           (make-number [1 2])))))
+
+
+(deftest sum-signals-test
+  (testing "Given pairs of digits, find the sums of the numbers they make"
+    (is (= 142
+           (calibration-value example-data)))))
+
+(def input-data
+  (str/split
+   (slurp "input-1.txt")
+   #"\n"))
+
+(deftest part-1-test
+  (testing "Part one is answered correctly for the input data"
+    (is (= 55130
+           (calibration-value input-data)))))
+
+;; Part 2
+
+(deftest words-to-integers-test
+  (testing "parsing integers from their names"
+    (is (= 1 (wtoi "one")))
+    (is (= 2 (wtoi "two")))))
+
+(deftest words-or-ints-to-integers-test
+  (testing "strings can be digits or words, parse them to integers"
+    (is (= 7 (atoi "seven")))
+    (is (= 5 (atoi "five")))
+    (is (= 3 (atoi "3")))))
+
+(def part-2-example-data
+  (str/split 
+   "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"
+   #"\n"))
+
+(deftest first-last-with-words-test
+  (testing "Finding the first and last digit in a line, including word tokens"
+    (is (= [2 9]
+           (first-and-last-with-words (first part-2-example-data)))
+        (= [7 6]
+           (first-and-last-with-words (last part-2-example-data)))))
+  (testing "a string with digit words sharing a letter should be separated correctly"
+    (is (= [1 8]
+           (first-and-last-with-words "oneight")))))
+
+
+(deftest sum-signals-words-test
+  (testing "Given pairs of digits including words, find the sums of the numbers they make"
+    (is (= 281
+           (calibration-value-with-words part-2-example-data)))))
+
+(deftest part-2-test
+  (testing "Part one is answered correctly for the input data"
+    (is (= 54985
+           (calibration-value-with-words input-data)))))
