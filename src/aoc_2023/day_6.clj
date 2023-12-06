a5955bf5d492acd3e7cca23187913a78ae086c39
Joel <joel@gluth.io>
2023-12-06 13:03:59 +0000

    Day 6 suspiciously gentle.

--- src/aoc_2023/day_6.clj
+++ src/aoc_2023/day_6.clj
@@ -1,3 +1,16 @@
 (ns aoc-2023.day-6
   (:require [aoc-2023.core :refer :all]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.math :as m]))
+
+; y = -x^2 + rx
+(defn distance-travelled [hold race-duration]
+  (* hold (- race-duration hold)))
+
+;; lowest and highest hold times that will win a race
+(defn winning-range [race-duration best-time]
+  (let [delta (m/sqrt (- (* race-duration race-duration) (* 4 best-time)))]
+    ;; make sure you are strictly greater/less than numbers that hit
+    ;; the record exactly
+    [(int (m/floor  (inc (/ (- race-duration delta) 2))))
+     (int (m/ceil   (dec (/ (+ race-duration delta) 2))))]))
--- test/aoc_2023/day_6_test.clj
+++ test/aoc_2023/day_6_test.clj
@@ -14,6 +14,44 @@
   {:time     [7 15  30]
    :distance [9 40 200]})
 
-(deftest a-test
-  (testing "failure"
-    (is (= 0 1))))
+(deftest distance-test
+  (testing "distances for various hold and race times are calculated"
+    (is (=  0 (distance-travelled 0 7)))
+    (is (=  6 (distance-travelled 1 7)))
+    (is (= 10 (distance-travelled 2 7)))
+    (is (=  0 (distance-travelled 7 7)))))
+
+(deftest winning-ranges-test
+  (testing "minimum and maximum hold times for a race to beat a given distance are calculated"
+    (is (= [ 2  5] (winning-range  7   9)))
+    (is (= [ 4 11] (winning-range 15  40)))
+    (is (= [11 19] (winning-range 30 200)))))
+
+(deftest total-ways-test
+  (testing "The total number of ways to win a series of races multiply correctly"
+    (let [races (zipmap (:time example-data)
+                        (:distance example-data))]
+      (is (= 288
+             (->> races 
+                  (map #(apply winning-range %))
+                  (map #(apply - %))
+                  (map dec)
+                  (map abs)
+                  (reduce *)))))))
+
+(deftest part-1-test
+  (let [races (zipmap (:time input-data)
+                      (:distance input-data))]
+      (is (= 2374848
+             (->> races 
+                  (map #(apply winning-range %))
+                  (map #(apply - %))
+                  (map dec)
+                  (map abs)
+                  (reduce *))))))
+
+;; part 2
+
+(deftest part-2-test
+  (is (= 39132886
+         (abs (dec (apply - (winning-range 55999793 401148522741405)))))))
