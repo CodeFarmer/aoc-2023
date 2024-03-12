8be7c192fe88080b1ee0e3555be7892794ef1b97
Joel <joel@gluth.io>
2024-03-12 21:10:22 +0000

    Day 8 part 2

--- src/aoc_2023/day_8.clj
+++ src/aoc_2023/day_8.clj
@@ -26,3 +26,32 @@
                                    :default (throw (RuntimeException. (str "Unexpected direction '" d "'"))))]
                     (recur (inc c) (rest directions) original-directions graph dest end-node)))))
 
+;; TODO refactor count-steps in terms of next-move
+(defn next-move [graph d start-node]
+  (let [[l r] (graph start-node)]
+    (cond (= d \L) l
+          (= d \R) r)))
+
+(defn count-steps-until-all
+  ([directions graph start-nodes pred]
+   (count-steps-until-all 0 directions directions graph start-nodes pred))
+  ([c directions original-directions graph start-nodes pred]
+   (cond (every? pred start-nodes) c
+         (empty? directions) (recur c original-directions original-directions graph start-nodes pred)
+         :default (recur (inc c) (rest directions) original-directions graph (into [] (map #(next-move graph (first directions) %) start-nodes)) pred))))
+
+;; exploring the problem space
+(defn probe-cycles [c directions original-directions graph start-node pred]
+  (cond (and (not (zero? c)) (pred start-node)) [c start-node directions]
+        (empty? directions) (recur c original-directions original-directions graph start-node pred)
+        :default (recur (inc c) (rest directions) original-directions graph (next-move graph (first directions) start-node) pred)))
+
+(defn ends-with-z? [astr]
+  (.endsWith astr "Z"))
+
+(defn ends-with-a? [astr]
+  (.endsWith astr "A"))
+
+;; given two BigIntegers, find the lowest common multiple
+(defn lcm [a b]
+  (.multiply a (.divide b (.gcd a b))))
--- test/aoc_2023/day_8_test.clj
+++ test/aoc_2023/day_8_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [aoc-2023.day-8 :refer :all]
             [aoc-2023.core :refer :all]
-            [clojure.string :as str]))
+            [clojure.string :as str])
+  (:import (java.math BigInteger)))
 
 (def sample-data "RL
 
@@ -42,7 +43,47 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)")]
       (is (= 6 (count-steps directions graph "AAA" "ZZZ"))))))
 
+(def input-data (slurp "aoc-2023-inputs/input-8.txt"))
 (deftest part-1-test
-  (let [input-data (slurp "aoc-2023-inputs/input-8.txt")
-        [directions graph] (parse-data-string input-data)]
-    (is (= 0 (count-steps directions graph "AAA" "ZZZ")))))
+  (let [[directions graph] (parse-data-string input-data)]
+    (is (= 19637 (count-steps directions graph "AAA" "ZZZ")))))
+
+;; part 2
+
+(def sample-data-2 "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)")
+
+(deftest thunk-test
+  (let [[directions graph] (parse-data-string sample-data-2)]
+    (is (= ["11B" "22B"] (map #(next-move graph \L %) ["11A" "22A"])))))
+
+(deftest count-while-not-all-terminated-test
+  (let [[directions graph] (parse-data-string sample-data-2)]
+    (is (= 6 (count-steps-until-all directions graph ["11A" "22A"] #(.endsWith % "Z"))))))
+
+(deftest convenient-graph-shape-test
+  (let [[directions graph] (parse-data-string input-data)
+        start-nodes (filter ends-with-a? (keys graph))
+        initial-probes (map #(probe-cycles 0 directions directions graph % ends-with-z?) start-nodes)
+        subsequent-probes (map #(probe-cycles 0 directions directions graph % ends-with-z?) (map second initial-probes))]
+    
+    (testing "the length of each cycle from the end node to itself is the same as the length of the journey to get to it from a start node, and is also always a multiple of the length of the LR instructions - which means the LCM of the initial journeys is the first time they will all terminate"
+      (is (every? empty? (map last initial-probes))
+          "Each initial journey should empty the instruction list exactly")
+      (is (= initial-probes subsequent-probes)
+          "Each initial journey and loop should end at the same node and take the same number of steps"))
+
+    (testing "LCM of the initial journey lengths is the number of moves taken to reach simultaneous termination"
+      (is (= 8811050362409 (->> initial-probes
+                                (map first)
+                                (map #(BigInteger. (str %)))
+                                (reduce lcm)))))))
+
