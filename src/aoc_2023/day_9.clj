42fec6209da2179d0c57a5bdd76dd89d1d4a9d20
Joel <joel@gluth.io>
2024-03-13 22:19:45 +0000

    Day 9 part 1

--- src/aoc_2023/day_9.clj
+++ src/aoc_2023/day_9.clj
@@ -3,15 +3,23 @@
             [clojure.string :as str]))
 
 (defn parse-int-line [astr]
-  (intify-seq (re-seq #"\d+" astr)))
+  (intify-seq (re-seq #"-?\d+" astr)))
 
 (defn diff-seq [num-seq]
   (map (fn [[a b]] (- b a)) (partition 2 1 num-seq)))
 
 (defn repeated-diff
   ([num-seq]
-   (repeated-diff [] num-seq))
+   (repeated-diff '() num-seq))
   ([acc num-seq]
    (if (every? zero? num-seq)
      (conj acc num-seq)
      (recur (conj acc num-seq) (diff-seq num-seq)))))
+
+(defn predict-next
+  ([seqs]
+   (predict-next 0 seqs))
+  ([p seqs]
+   (cond (empty? seqs) p
+         (empty? (first seqs)) p
+         :default (recur (+ p (last (first seqs))) (rest seqs)))))
--- test/aoc_2023/day_9_test.clj
+++ test/aoc_2023/day_9_test.clj
@@ -18,8 +18,25 @@
     (is (= [3 3 3 3 3]
            (diff-seq [0 3 6 9 12 15])))))
 
+;; note this is upside-down compared to the example, for easier traversal
 (deftest repeated-diffing-test
-  (is (= [[0 3 6 9 12 15]
+  (is (= [[  0 0 0 0]
           [ 3 3 3 3  3]
-          [  0 0 0 0]]
+          [0 3 6 9 12 15]]
          (repeated-diff [0 3 6 9 12 15]))))
+
+(deftest prediction-test
+  (testing "Given a sequentially-subtracted sequence of number sequences, the correct next number is predicted for the last sequence"
+    (is (= 18 (predict-next (repeated-diff [0 3 6 9 12 15]))))
+    (is (= 28 (predict-next (repeated-diff [1 3 6 10 15 21]))))
+    (is (= 68 (predict-next (repeated-diff [10 13 16 21 30 45]))))))
+
+(def input-data
+  (map parse-int-line
+       (str/split (slurp "aoc-2023-inputs/input-9.txt")
+                  #"\n")))
+
+(deftest part-1-test
+  (is (= 1581679977 (reduce + (->> input-data
+                                   (map repeated-diff)
+                                   (map predict-next))))))
