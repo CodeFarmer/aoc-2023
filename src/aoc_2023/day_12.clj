3d323ab6ed0ad3afe039416ca93011bb88e2e519
Joel <joel@gluth.io>
2024-06-19 14:17:41 +0100

    Picking up from day 12 midyear, Let's start this again

--- src/aoc_2023/day_12.clj
+++ src/aoc_2023/day_12.clj
@@ -14,51 +14,8 @@
             (> n 0)  (recur (conj acc n) 0 (rest astr))
             :default (recur acc 0 (rest astr))))))
 
-(defn -check-arrangements [aseq rseq] (every? #(= aseq (-derive-arrangements [] 0 %)) rseq))
-
-; (every? #(= len (count %)) %)
-
-(defn arrangements [len aseq]
-  (comment {:post [(-check-arrangements aseq %)
-                   ]})
-  (if (empty? aseq)
-    [(apply str (repeat len \.))]
-    (let [n (first aseq)
-          block (apply str (repeat n \#))
-          others (rest aseq)
-          required-space (+ (count others) (reduce + others))]
-      
-      (reduce into []
-              (for [pad-len (range 0 (- len required-space))
-                    :let [padding (apply str (repeat pad-len \.))
-                          prefix  (str padding block)
-                          prefix-len (+ pad-len n)]]
-                (cond (> prefix-len len) [] ;; one recursion endpoint, failure
-                      (and (= prefix-len len) (empty? others)) [prefix] ;; other recursion endpoint, fill to the end
-                      :default (map #(str prefix "." %) (arrangements (- len (inc prefix-len)) others))))))))
-
-(defn *arrangements [len aseq]
-  (let [r (arrangements len aseq)]
-    (println "(arrangements " len aseq "): " r)
-    r))
 
 (defn parse-line [astr]
   (let [[rec runstr] (str/split astr #" ")
         runs (intify-seq (str/split runstr #","))]
     [rec runs]))
-
-(defn compatible? [pattern astr]
-  (if (empty? pattern) (empty? astr)
-      (let [p (first pattern)]
-        (cond (= p \?) (compatible? (rest pattern) (rest astr))
-              (= p (first astr)) (compatible? (rest pattern) (rest astr))
-              :default false))))
-
-(defn count-compatibles [astr avec]
-  (count (filter (partial compatible? astr)
-                 (arrangements (count astr) avec))))
-
-(defn *count-compatibles [astr avec]
-  (let [ret (count-compatibles astr avec)]
-    (println "count-compatibles:" astr avec ":" ret)
-    ret))
--- test/aoc_2023/day_12_test.clj
+++ test/aoc_2023/day_12_test.clj
@@ -5,35 +5,11 @@
             [clojure.string :as str]))
 
 (deftest arrangement-derivation-test
-  (testing "For debugging purposes, the list of runs can be derviced from an arrangement string"
+  (testing "For debugging purposes, the list of runs can be derived from an arrangement string"
     (is (= [1 1 3] (-derive-arrangements [] 0 "#.#..###")))))
-
-(deftest arrangement-test
-  (testing "arrangements of one group"
-    (is (= [] (arrangements 3 [4]))
-        "There are no arrangements when the run length is longer than the available space")
-    (is (= ["..."] (arrangements 3 []))
-        "There is one blank arrangement when there are no run lengths")
-    (is (= ["###"] (arrangements 3 [3]))
-        "There is one arrangement when the only run length is the same as the available space")
-    (is (= ["###." ".###"] (arrangements 4 [3]))
-        "There are two arrangements with a single one-space gap")
-    
-    (is (= ["#.#"] (arrangements 3 [1 1]))
-        "There is one arrangement of two unit runs")
-    (is (= ["#.#." "#..#" ".#.#"] (arrangements 4 [1 1]))
-        "There are thhree arrangement of two unit runs in four spaces")
-    
-    (is (= ["#.##." "#..##" ".#.##"] (arrangements 5 [1 2])))
-    (is (= ["#.##.." "#..##." "#...##" ".#.##." ".#..##" "..#.##"] (arrangements 6 [1 2])))))
-
 
 (deftest input-parsing-test
   (is (= ["#.#.###" [1 1 3]] (parse-line "#.#.### 1,1,3"))))
-
-(deftest compatibility-test
-  (is (compatible? "???.###" "#.#.###"))
-  (is (not (compatible? "???.###" "#..####"))))
 
 (deftest compatibility-counting-test
   (is (= 4
@@ -58,13 +34,10 @@
 ?###???????? 3,2,1"
                   #"\n")))
 
-(defn total-compatibles [lines]
-  (reduce (fn [a [pattern runs]] (+ a (count-compatibles pattern runs)))
-          0
-          lines))
+(comment 
+  (deftest sample-test
+    (is (= 21 (total-compatibles sample-data)))))
 
-(deftest sample-test
-  (is (= 21 (total-compatibles sample-data))))
-
-(deftest part-1-test
-  (is (= 7633 (total-compatibles input-data))))
+(comment 
+  (deftest part-1-test
+    (is (= 7633 (total-compatibles input-data)))))
