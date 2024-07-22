ec4348374b63b53200fdc960312dcc87e4f1c52f
Joel <joel@gluth.io>
2024-07-22 22:31:11 +0100

    Day 15 part 2

--- src/aoc_2023/day_15.clj
+++ src/aoc_2023/day_15.clj
@@ -13,3 +13,17 @@
                              (* 17)
                              (rem 256))
                          (rest astr)))))
+
+(defn process-command [boxes astr]
+  (let [[_ tag arg] (re-find (re-matcher #"(\w+)[=-](\d+)?" astr))
+        h (hhash tag)
+        box (boxes h)]
+    
+    (assoc boxes h (if arg (assoc box tag (Integer/parseInt arg))
+                       (dissoc box tag)))))
+
+(defn box-score [index amap]
+  (let [ks (keys amap)]
+    (->> (range 0 (count ks))
+         (map #(* (inc index) (inc %) (get amap (nth ks %))))
+         (reduce +))))
--- test/aoc_2023/day_15_test.clj
+++ test/aoc_2023/day_15_test.clj
@@ -4,17 +4,63 @@
             [aoc-2023.core :refer :all]
             [clojure.string :as str]))
 
-(def sample-data "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
+(def sample-data
+  (str/split "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+             #","))
 
 (deftest holiday-hash-test
   (is (= 0 (hhash "")))
   (is (= (hhash "HASH") 52))
-  (let [sample-commands (str/split sample-data #",")]
-    (is (= 1320
-           (reduce + (map hhash sample-commands))))))
+  (is (= 1320
+         (reduce + (map hhash sample-data)))))
 
-(def input-data (slurp "aoc-2023-inputs/input-15.txt"))
+(def input-data
+  (str/split (slurp "aoc-2023-inputs/input-15.txt")
+             #","))
 
 (deftest part-1-test
   (is (= 504036
-         (reduce + (map hhash (str/split input-data #","))))))
+         (reduce + (map hhash input-data)))))
+
+;; part 2
+
+;; (array-map) has exactly the key ordering properties the text calls
+;; for
+(deftest lens-placing-test
+  (let [boxes (into [] (repeat 4 (array-map)))]
+    (is (= [{"rn" 1}
+            {}
+            {}
+            {}]
+           (process-command boxes "rn=1"))))
+ 
+  (let [boxes [{"rn" 1 "cm" 2}
+               {"qp" 3}]]
+    (is (= [{"rn" 1 "cm" 2}
+            {}]
+           (process-command boxes "qp-"))))
+
+  (let [boxes (into [] (repeat 4 (array-map)))]
+    (is (= [{"rn" 1 "cm" 2}
+            {}
+            {}
+            {"ot" 7 "ab" 5 "pc" 6}]
+
+           (reduce process-command boxes sample-data)))))
+
+(deftest focal-power-test
+  (is (= 5 (box-score 0 {"rn" 1 "cm" 2})))
+  (let [boxes (into [] (repeat 4 (array-map)))
+        final-boxes (reduce process-command boxes sample-data)]
+    (is (= 145
+           (->> (range 0 (count final-boxes))
+                (map #(box-score % (get final-boxes %)))
+                (reduce +))))))
+
+(deftest part-2-test
+  (let [boxes (into [] (repeat 256 (array-map)))
+        final-boxes (reduce process-command boxes input-data)]
+    (is (= 295719
+           (->> (range 0 (count final-boxes))
+                (map #(box-score % (get final-boxes %)))
+                (reduce +))))))
